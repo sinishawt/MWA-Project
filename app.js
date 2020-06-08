9d94013018ed64bd7e2a105445862a0ee8785f01
memarZe <61173549+memarZe@users.noreply.github.com>
2020-06-07 19:55:39 -0500

    corse error handling

--- app.js
+++ app.js
@@ -13,6 +13,7 @@ app.use(morgan('dev'));
 app.use(bodyParser.urlencoded({extended:false}));
 app.use(bodyParser.json());
 
+// CORS error handling
 app.use((req, res, next) =>{
     res.header('Access-Control-Allow-Origin', '*')
     res.header(
