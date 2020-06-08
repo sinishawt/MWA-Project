bd0cf41e022450eb4bf6492ae1af799508ad37e9
memarZe <61173549+memarZe@users.noreply.github.com>
2020-06-08 14:19:31 -0500

    writing controller

--- api/routes/product_route.js
+++ api/routes/product_route.js
@@ -2,117 +2,13 @@ const express = require('express');
 const router = express.Router();
 const Product = require('../models/Product')
 const mongoose = require('mongoose');
+const productController = require('../controller/product_controller');
 
-router.get('/', (req, res, next)=>{
-    Product.find()
-            .select('title price _id status imageName descreption')
-            .exec()
-            .then(docs => {
-                console.log(docs);
-                const response ={
-                    count: docs.length,
-                    products: docs
-                };
-               // if(docs.length >= 0){
-                    res.status(200).json(response);
-                // } else{
-                //     res.status(404).json({
-                //         message: 'No entries found'
-                //     });
-                // }
-            })
-            .catch(err => {
-                console.log();
-                res.status(500).json({
-                    error: err
-                });
-            });
-});
-router.post('/', (req, res, next)=>{
-   
-    const product = new Product({
-        _id: new mongoose.Types.ObjectId(),
-        title: req.body.title,
-        catagoryId: req.body.catagoryId,
-        price: req.body.price,
-        status: req.body.status,
-        imageName: req.body.imageName,
-        descreption: req.body.descreption,
-        sellerId: req.body.sellerId
-    });
-
-    product.save()
-            .then(result => {
-                console.log(result);
-                res.status(200).json({
-                    message: 'Handling POST Request!',
-                    createdProduct: result
-                });
-            })
-            .catch(err => {
-                console.log(err);
-                res.status(500).json({
-                    error: err
-                })
-            });
-
-    
-    
-});
-
-router.get('/:productId', (req, res, next)=>{
-    const id = req.params.productId;
-    Product.findById(id)
-            .exec()
-            .then(doc => {
-                console.log("Data from the database", doc);
-                if(doc){
-                    res.status(200).json(doc);
-                } else{
-                    res.status(404).json({message: 'No Valid Entry Found or Provided'});
-                }
-            })
-            .catch(err => {
-                console.log(err);
-                res.status(500).json({error: err})
-            });
-
-});
-
-router.patch('/:productId', (req, res, next)=>{
-    const id = req.params.productId;
-    const updateOps = {};
-    for(const ops of req.body){
-        updateOps[ops.propName] = ops.value;
-    }
-    Product.update({_id: id}, {$set: updateOps})
-            .exec()
-            .then(result => {
-                console.log(result);
-                res.status(200).json(result);
-            })
-            .catch(err => {
-                console.log(err);
-                res.status(500).json({
-                    error: err
-                })
-            });
-});
-
-router.delete('/:productId', (req, res, next)=>{
-    const id = req.params.productId;
-    Product.remove({_id: id})
-            .exec()
-            .then(result => {
-                res.status(200).json(result);
-            })
-            .catch(err => {
-                console.log(err);
-                res.status(500).json({
-                    error: err
-                });
-            });
-});
+router.get('/', productController.list);
+router.post('/', productController.insert);
+router.get('/:productId', productController.findProduct);
+router.patch('/:productId', productController.updateProduct);
+router.delete('/:productId', productController.deleteProduct);
 
 
 module.exports = router;
