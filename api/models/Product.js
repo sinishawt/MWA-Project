621aa616dbb7bb40f0d65e3cd1e9aaff2df9ec1f
memarZe <61173549+memarZe@users.noreply.github.com>
2020-06-08 12:29:53 -0500

    Data Validation

--- api/models/Product.js
+++ api/models/Product.js
@@ -1,13 +1,13 @@
 const mongoose = require('mongoose');
 const productSchema = mongoose.Schema({
     _id: mongoose.Schema.Types.ObjectId,
-    title: String,
-    catagoryId: Number,
-    price: Number,
-    status: String,
-    imageName: String,
-    descreption: String,
-    sellerId: String
+    title: { type: String, required: true},
+    catagoryId: {type: Number, required: true},
+    price: {type: Number, required: true },
+    status: {type: String, required: true},
+    imageName: {type: String, required: true},
+    descreption: {type: String, required: true},
+    sellerId: {type: String, required: true},
 });
 
 module.exports = mongoose.model('product', productSchema);
