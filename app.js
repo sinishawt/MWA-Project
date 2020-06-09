734c14bb6492b3042e1ff0f4c29558875be89d69
birhanegg <59857947+birhanegg@users.noreply.github.com>
2020-06-09 17:53:03 -0500

    auth module skeleton

--- app.js
+++ app.js
@@ -9,25 +9,27 @@ const orderRoutes = require('./api/routes/order_route');
 const reviewRoutes = require('./api/routes/review_route');
 const adminRoutes = require('./api/routes/admin_route');
 const buyerRoutes = require('./api/routes/buyer_route');
+//const authRoutes = require('./api/routes/auth_route');
 
 
 app.use(morgan('dev'));
-app.use(bodyParser.urlencoded({extended:false}));
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 // CORS error handling
-app.use((req, res, next) =>{
+app.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', '*')
     res.header(
-        "Access-Control-Allow-Headers", 
+        "Access-Control-Allow-Headers",
         "Origin, X-Requested-With, Content-Type, Accept, Authorization");
-    if(req.method==='OPTIONS'){
+    if (req.method === 'OPTIONS') {
         res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET')
         return res.status(200).json({});
     }
     next();
 });
 //routes which handle requests
+//app.use('/auth', authRoutes);
 app.use('/buyer', buyerRoutes);
 app.use('/products', productRoutes);
 app.use('/order', orderRoutes);
@@ -40,7 +42,7 @@ app.use((req, res, next) => {
     error.status = 404;
     next(error);
 });
-app.use((error, req, res, next) =>{
+app.use((error, req, res, next) => {
     res.status(error.status || 500);
     res.json({
         error: {
