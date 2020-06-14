5b99ba4eb9056911c761ca0c105898f59a2eb46e
memarZe <61173549+memarZe@users.noreply.github.com>
2020-06-14 13:47:47 -0500

    shopping cart update

--- app.js
+++ app.js
@@ -4,7 +4,6 @@ const morgan = require('morgan');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 let session = require('express-session');
-
 let MongoStore = require('connect-mongo')(session);
 
 const productRoutes = require('./api/routes/product_route');
