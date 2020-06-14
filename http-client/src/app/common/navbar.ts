7cf297286c0fd5a3e79095c098cd3bf064e467f9
memarZe <61173549+memarZe@users.noreply.github.com>
2020-06-14 15:10:06 -0500

    client and server name change

--- http-client/src/app/common/navbar.ts
+++ http-client/src/app/common/navbar.ts
@@ -13,12 +13,12 @@ import { Component, OnInit } from '@angular/core';
             <div class="collapse navbar-collapse" id="navbarResponsive">
                 <ul class="navbar-nav ml-auto">
                     <li class="nav-item active">
-                        <a class="nav-link" href="#">Home
+                        <a class="nav-link" href="#"> Home
                             <span class="sr-only">(current)</span>
                         </a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="#">Cart</a>
+                        <a class="nav-link" href="#"> Cart</a>
                     </li>
                     <li class="nav-item">
                         <a class="nav-link" href="#">Orders</a>
