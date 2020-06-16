436448bdbad02a24f1149e88aeedf8f57e180ca9
birhanegg <59857947+birhanegg@users.noreply.github.com>
2020-06-16 10:47:31 -0500

    notification link

--- client/src/app/common/sidebar.buyer.ts
+++ client/src/app/common/sidebar.buyer.ts
@@ -4,7 +4,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-side-buyer',
   template: `
-  <h1 class="my-4">Welcome Buyer</h1>
+  <h3 class="my-4">Welcome Buyer</h3>
                 <div class="list-group">
                     <a href="buyer/" class="list-group-item">Home</a>
                     <a href="buyer/view-cart" class="list-group-item">View Cart</a>
