baa42a6e33d5c05ba75ba7ae4d3ed3706773d1ca
memarZe <61173549+memarZe@users.noreply.github.com>
2020-06-17 15:03:23 -0500

    place order

--- server/api/models/Order.js
+++ server/api/models/Order.js
@@ -7,8 +7,19 @@ const orderSchema = mongoose.Schema({
     //_id: mongoose.Schema.Types.ObjectId,
     productId: { type: mongoose.Schema.Types.ObjectId, required: true},
     buyerId: { type: mongoose.Schema.Types.ObjectId, required: true},
+    sellerId: { type: mongoose.Schema.Types.ObjectId, required: true},
     status: { type: String, required: true},
-    orderDate: {type: Date, required: true}
+    orderDate: {type: Date, required: true},
+    shippedDate: {type: Date, required: false},
+    deliveredDate: {type: Date, required: false},
+    shippingAddress: {
+        zipCode: {type: Number, required: false},
+        street: { type: String, required : false},
+        city: { type: String, required : false},
+        state: { type: String, required : false},
+        phoneNo: { type: Number, required : false},
+        country: { type: String, required : false}
+    }
 });
 
 //const orderSchema = mongoose.Schema({
