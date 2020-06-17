c2bea1bc13aeb43e4334dfeb89cd94a110f25d55
sinishawt <59536151+sinishawt@users.noreply.github.com>
2020-06-17 16:29:27 -0500

    home page moved

--- client/src/app/components/buyer/buyer.component.ts
+++ client/src/app/components/buyer/buyer.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import {ProductService} from '../../services/product.service';
+import {buyerService} from '../../services/buyer.service';
+import {Product} from '../../common/product';
+import { Router } from "@angular/router";
 
 @Component({
   selector: 'app-buyer',
@@ -7,9 +11,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BuyerComponent implements OnInit {
 
-  constructor() { }
+  products: Product[];
+  constructor(private router: Router, private productService: ProductService, private buyerService :buyerService) { }
 
-  ngOnInit(): void {
+  ngOnInit() {
+    this.listProducts();
+  }
+
+   listProducts() {
+    this.productService.getProductList().subscribe(
+          data => { this.products = data; }
+    );
+  }
+
+  addCart(items: Product)  {
+      this.buyerService.addToShoppingCart(items._id).subscribe(res => res);
+      console.log(items._id);
+  }
+
+  viewDetail(id : any) {
+    window.localStorage.removeItem("productId");
+    window.localStorage.setItem("productId", id.toString());
+    this.router.navigate(['viewProduct']);
   }
 
 }
