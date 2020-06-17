563228f48d5c712d3a790627d9e698bb91d24ae8
Binyam-Kahsay <benj.zone@gmail.com>
2020-06-17 10:50:01 -0500

    order and cron

--- server/api/cron/cron.js
+++ server/api/cron/cron.js
@@ -1,21 +1,15 @@
 // const cron = require("node-cron");
 //
-//
-//
 // const User = require("../models/Buyer");
 //
 // const orderModel = require("../models/Order")
 //
-//
-//
 // exports.run = () => {
-//     //all your task here
+//
 //     cron.schedule("* * * * * ", function() {
 //
 //
-//             console.log(orderModel.status)
-//
-//
+//         console.log(orderModel.status)
 //
 //         orderModel.find()
 //
@@ -29,56 +23,28 @@
 //
 //
 //
-//                     if(order.status === "delivered") {
+//                         if(dateDifference > 1000000 && order.status=="Ordered" ){
 //
-//
-//                         if(dateDifference > 1000000 ){
+//                             console.log('produid', order.buyerId)
 //
 //                             User.updateOne({_id :order.buyerId}, {$inc:{gainedPoints:111}} ,function (err,data){
+//                                 console.log(err)
+//                             })
+//
+//                             orderModel.updateOne({_id :order._id}, {status:'Done'} ,function (err,data){
 //                                 console.log(err)
 //                             })
 //
 //                         }
 //
 //
-//
-//
-//                     }
-//
-//
-//
-//
-//                     //console.log(req.user._id)
-//
-//
-//
-//                     // console.log('user', User.findById())
-//                     //
-//                     // console.log("price ; ", order.price);
-//                     //
 //                     console.log("time difference ; ", dateDifference);
-//                     // console.log("time Now : ",  dateNow);
-//                     // console.log("-------------------");
-//                 }); //
+//
+//                 });
 //
 //             })
 //             .catch(err => console.log(err));
 //
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//         //1. get all orders with status=delivered
-//         //2. for each order calculate diff
-//             //3. if diff >= 10day
-//             //4. add user.gain increment by 10% of price and update order.status=closed
-//             //loop everyday
 //
 //
 //         console.log("---------------------");
