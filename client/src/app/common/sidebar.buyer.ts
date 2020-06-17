4726051d82c8768f4d0ab4532625a6ded5b7579f
sinishawt <59536151+sinishawt@users.noreply.github.com>
2020-06-16 19:59:26 -0500

    added buyer pages missing

--- client/src/app/common/sidebar.buyer.ts
+++ client/src/app/common/sidebar.buyer.ts
@@ -8,9 +8,10 @@ import { Component, OnInit } from '@angular/core';
                 <div class="list-group">
                     <a href="buyer/" class="list-group-item">Home</a>
                     <a href="buyer/view-cart" class="list-group-item">View Cart</a>
-                    <a href="" class="list-group-item">Shipping Address Orders</a>
-                    <a href="" class="list-group-item">My Orders</a>
-                    <a href="" class="list-group-item"> Payment Settings</a>
+                    <a href="buyer/view-orders" class="list-group-item">My Orders</a>
+                    <a href="buyer/shipping-address" class="list-group-item">Shipping Address </a>
+                    <a href="buyer/payment-settings" class="list-group-item"> Payment Settings</a>
+                    <a href="buyer/notifications" class="list-group-item"> Payment Settings</a>       
                 </div>
   `,
   styles: []
---
+++ client/src/app/components/buyer/view-orders/view-orders.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-view-orders',
+  templateUrl: './view-orders.component.html',
+  styleUrls: ['./view-orders.component.css']
+})
+export class ViewOrdersComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  printToPdf(): void {
+    
+  }
+
+}
